feaf86a567be43cf2c6d985b82fba964a94f6895
James Prytherch <jamesp802@hotmail.com>
2020-09-06 11:29:18 -0400

    Front End Sign Up Form
    - changed App.js switch routes, private routes implementation INCOMPLETE
    - rough outline and styling
    - submits and creates a new user
    - logs resulting from backend (token)
    
    todo: private routes, redirects if authenticated, login page, combine GitHub OAuth and login

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import NavBar from "./components/navigation/navbar";
 import UserDashBoard from "./components/user/userDashBoard";
-import GitSignIn from "./components/auth/gitSignIn"
+import GitSignIn from "./components/auth/gitSignIn";
+import UserSignUp from "./components/auth/signup";
 
 class App extends React.Component {
   render() {
@@ -12,7 +13,13 @@ class App extends React.Component {
         <NavBar />
         <Router>
           <Switch>
-          <Route path="/user/signin">
+            <Route path="/login">
+              <GitSignIn />
+            </Route>
+            <Route path="/signup">
+              <UserSignUp />
+            </Route>
+            <Route path="/oauth">
               <GitSignIn />
             </Route>
             <Route path="/">
---
+++ src/components/auth/signup.js
@@ -0,0 +1,134 @@
+import React from "react";
+import axios from "axios";
+
+import styled from "styled-components";
+import { Form, Button } from "react-bootstrap";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 50px auto 50px;
+  grid-template-rows: 100px auto 100px;
+`;
+
+const FormContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  grid-column-start: 2;
+  grid-row-start: 2;
+`;
+
+const ErrorMessage = styled.span`
+  color: red;
+`;
+
+class UserSignUp extends React.Component {
+  state = {
+    formBasicEmail: "",
+    formBasicPassword: "",
+    formBasicPasswordCheck: "",
+    formBasicUsername: "",
+    validPassword: true,
+    errorMessage: "",
+  };
+
+  componentDidMount() {
+    console.log(this.state);
+  }
+
+  handleChange = (e) => {
+    const { id, value } = e.target;
+    this.setState({
+      [id]: value,
+    }, console.log(this.state));
+  };
+
+  handleSubmit = () => {
+    const {
+      formBasicEmail,
+      formBasicPassword,
+      formBasicPasswordCheck,
+      formBasicUsername,
+      validPassword,
+      errorMessage,
+    } = this.state;
+
+    if (formBasicPassword !== formBasicPasswordCheck) {
+      this.setState({
+        validPassword: false,
+        errorMessage: "Passwords Must Match",
+      });
+      return;
+    } else if (validPassword === true) {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const registration = {
+        username: formBasicUsername,
+        email: formBasicEmail,
+        password: formBasicPassword,
+      };
+
+      axios
+        .post("/user/signup", registration, config)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
+  render() {
+    const { errorMessage } = this.state;
+    return (
+      <Container>
+        <FormContainer>
+          <Form>
+            {errorMessage === "" ? null : (
+              <ErrorMessage>{errorMessage}</ErrorMessage>
+            )}
+            <Form.Group
+              controlId="formBasicUsername"
+              onChange={this.handleChange}
+            >
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="username" placeholder="Enter a username" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicEmail" onChange={this.handleChange}>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group
+              controlId="formBasicPassword"
+              onChange={this.handleChange}
+            >
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group
+              controlId="formBasicPasswordCheck"
+              onChange={this.handleChange}
+            >
+              <Form.Label>Re-Enter Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" onClick={this.handleSubmit}>
+              Sign Up
+            </Button>
+          </Form>
+        </FormContainer>
+      </Container>
+    );
+  }
+}
+
+export default UserSignUp;
