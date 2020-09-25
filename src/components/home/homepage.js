0fc5c2935dcf439beb40ecdf484b971a0819dbdc
James Prytherch <jamesp802@hotmail.com>
2020-09-25 15:12:14 -0400

    homepage tutorial video post, removed link to dev notes temp

--- src/components/home/homepage.js
+++ src/components/home/homepage.js
@@ -12,24 +12,28 @@ class Home extends React.Component {
         style={{
           backgroundColor: "#0e1217",
           color: "white",
+          textAlign: "center",
+          paddingTop: "0px"
         }}
       >
-        <img
+        {/* <img
           src="https://media.giphy.com/media/l0HlTy9x8FZo0XO1i/giphy.gif"
           style={{ display: "block", margin: "auto" }}
+        /> */}
+
+        <img
+          src="https://media.giphy.com/media/l0HlTy9x8FZo0XO1i/giphy.gif"
+          style={{ float: "center", width: "250px", height: '100px' }}
         />
-        <h1>
-          Pyrotech.io Fire Ticketing System <b>ALPHA 1.0</b>
-        </h1>
+        <h1>Pyrotech.io Ticketing System</h1>
         <p>
           An Agile Software Development Tool for Personal, and Team Projects.
           <br />
           <br />
+          <b>Released: 9/25/2020</b>
           This application is in development
         </p>
-        <p>
-          <Button variant="primary">Learn more</Button>
-        </p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/b5JsNdf3-Vk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" style={{ display: "block", margin: "auto", padding: "10px" }} allowfullscreen></iframe>
       </div>
     );
   }
